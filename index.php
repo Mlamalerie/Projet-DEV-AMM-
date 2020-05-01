86047562674bb1bf1d046754034fd53fc9ff40d2
Ari <arihoela@gmail.com>
2020-05-01 18:16:27 +0200

    Ajout listes des producteurs

--- index.php
+++ index.php
@@ -32,7 +32,13 @@
                         <h1 class="display-3">Développez vos sons</h1>
                         <p class="lead mb-0">Découvrez et Partagez les prods de vos choix</p>
                         <p class="lead mb-0">Pour pouvoir acheter ou vendre des prods  <a href="inscription.php"><button type="button" class="btn btn-danger" >Inscrivez-vous</button></a></p>
-                    </div>
+                    </div><br/>
+                  <!-- test barre de recherche <form class="Recherche">
+                      <div class="form-group">
+                        <input type="search" class="input-sm form-control" placeholder="musique, artistes,...">
+                        <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Rechercher</button>
+                      </div>
+                    </form> -->
                 </div>
             </div>
         </header>
@@ -82,6 +88,36 @@
                     <div class="col-lg-9 mx-auto">
                         <h2 class="h1 mb-4">Meilleur Producteur</h2>
                         <p class="font-italic mb-4 text-muted">Liste des profils des producteurs</p>
+                        <table class="table table-dark">
+                          <thead>
+                            <tr>
+                              <th scope="col">#</th>
+                              <th scope="col">First</th>
+                              <th scope="col">Last</th>
+                              <th scope="col">Handle</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th scope="row">1</th>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">2</th>
+                              <td>Jacob</td>
+                              <td>Thornton</td>
+                              <td>@fat</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">3</th>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                            </tr>
+                          </tbody>
+                        </table>
                         <a class="btn btn-outline-light px-4 rounded-0 scroll-top" href="#">Back to top</a>
                     </div>
                 </div>
