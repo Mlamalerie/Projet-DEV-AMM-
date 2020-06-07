9f47a9cb6e6ae9351141bb5630867f785fd6175d
Ari <arihoela@gmail.com>
2020-06-08 00:17:15 +0200

    maj liaison désac et messagerie.php

--- messagerie.php
+++ messagerie.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['user_id'])){/*si pas connecté*/
     exit;
 }
 
-$req = $BDD ->prepare("SELECT u.user_pseudo, u.user_id, m.message, m.date_message, m.id_from, m.id_to,m.lu
+$req = $BDD ->prepare("SELECT u.user_pseudo, u.user_id, u.user_image, u.user_statut, m.message, m.date_message, m.id_from, m.id_to, m.lu
     FROM(
    SELECT IF(r.id_demandeur=:id, r.id_receveur, r.id_demandeur) id_user, MAX(m.id) max_id 
     FROM relation r 
@@ -29,8 +29,8 @@ $req->execute(array('id'=>$_SESSION['user_id']));
 
 $afficher_conversation= $req ->fetchAll();
 
-$req1=$BDD->prepare("SELECT * FROM relation WHERE id_demandeur = ? AND statut = ?");
-$req1->execute(array($_SESSION['user_id'],3));   //pour enlever les bloqués de la messagerie
+$req1=$BDD->prepare("SELECT * FROM relation WHERE (id_demandeur = ? AND statut = ?) OR (id_receveur = ? AND statut = ?)");
+$req1->execute(array($_SESSION['user_id'],3,$_SESSION['user_id'],3));   //pour enlever les bloqués de la messagerie
 
 $relation_bloq=$req1->fetchAll(); 
 print_r($relation_bloq);
@@ -38,7 +38,7 @@ print_r($relation_bloq);
 
 <?php
 $okconnectey = false;
-if(isset($_SESSION['user_id']) || isset($_SESSION['user_pseudo'])  ) {
+if(isset($_SESSION['user_id']) || isset($_SESSION['user_pseudo'])) {
 
     $okconnectey = true;
 } else{
@@ -60,7 +60,21 @@ if(isset($_SESSION['user_id']) || isset($_SESSION['user_pseudo'])  ) {
         <link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
 
         <style>
-
+            .pseudo{
+                color: #7728b2;
+                    
+            }
+            .pseudo img{
+                height: 20px;
+                width: 20px;
+            }
+            .all_mssg{
+               border: 3px solid #7728b2;
+                border-radius: 20%;
+            }
+            tr:nth-child(even) {
+              background-color: rgb(230,235,255);
+            }
         </style>
 
     </head>
@@ -74,25 +88,30 @@ if(isset($_SESSION['user_id']) || isset($_SESSION['user_pseudo'])  ) {
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-                    <table>
+                   <h1>Mes messages</h1>
+                    <table class="all_mssg">
                         <?php
                        
                         foreach($afficher_conversation as $ac){
                             $okaffichemess = true;
-                            foreach($relation_bloq as $rb) {
-                             
-                                if($ac['user_id'] == $rb['id_receveur']) {
-                                    
+                            foreach($relation_bloq as $rb) { //enlever les personnes bloqués de notre messagerie                            
+                                if($ac['user_id'] == $rb['id_receveur']) {                                    
+                                    $okaffichemess = false;
+                                }
+                                else if($ac['user_id']==$rb['id_demandeur']){ // on enlève la personne qui nous a bloqué aussi
+                                     $okaffichemess = false;
+                                }
+                                else if($ac['user_statut'] == 0){// on enlève les profils désactivés
                                     $okaffichemess = false;
                                 }
                             }
-                            if($okaffichemess){ //enlever bloqués messagerie
+                            if($okaffichemess){ 
                         ?>
                         <tr>
                             <td>
 
-                                <a href="message.php?profil_id=<?= $ac['user_id'] ?>">
-                                    <?= $ac['user_pseudo'] ?>
+                                <a href="message.php?profil_id=<?= $ac['user_id'] ?>" class="pseudo">
+                                  <img src="<?= $ac['user_image'] ?>">  <?= $ac['user_pseudo'] ?>
                                 </a>
 
                             </td>
