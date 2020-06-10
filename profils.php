8ae794cac435fb34b9d3f93f24fbfcc273b460b3
Mlamali SAID SALIMO <saidsalimo@eisti.eu>
2020-06-10 15:15:13 +0200

    merge, carré

--- profils.php
+++ profils.php
@@ -5,13 +5,12 @@ $_SESSION['ici_index_bool'] = false;
 include('assets/db/connexiondb.php'); 
 
 
-/*print_r($_GET);*/print_r($_SESSION);
+/*print_r($_GET);*/
 
-if (!isset($_GET['profil_id'])){
-    echo "<script> history.go(-2); </script>"; 
-    exit;
-}
-
+//if (!isset($_GET['profil_id'])){
+//    header('Location: utilisateurs.php'); 
+//    exit;
+//}
 $okconnectey = false;
 if(isset($_SESSION['user_id']) || isset($_SESSION['user_pseudo'])) {
     $okconnectey = true;
@@ -24,7 +23,8 @@ if($okconnectey) {
 }
 
 
-    $req = $BDD->prepare("SELECT u.*
+
+$req = $BDD->prepare("SELECT u.*
         FROM user u
         WHERE u.user_id = :id1");
 
@@ -77,134 +77,82 @@ if(($afficher_profil['user_statut'] == 0)){
 
 }
 
+if(!empty($_POST)){
+    extract($_POST);
+    $valid=(boolean) true;
 
-    if(!empty($_POST)){
-        extract($_POST);
-        $valid=(boolean) true;
 
+    if(isset($_POST['follow'])){
+        echo "#follow";
+        $req = $BDD->prepare("SELECT id FROM relation WHERE (id_demandeur = ? AND id_receveur = ?)"); 
 
-        if(isset($_POST['follow'])){
-            echo "#follow";
-            $req = $BDD->prepare("SELECT id FROM relation WHERE (id_demandeur = ? AND id_receveur = ?)"); 
+        $req->execute(array( $id_demandeur,$id_receveur ));
 
-            $req->execute(array( $id_demandeur,$id_receveur ));
+        $verif_relation = $req->fetch();
 
-            $verif_relation = $req->fetch();
+        if(isset($verif_relation['id'])){/*follow existe*/
+            $valid=false;
+            echo "follow existe";
+        }
+        if($valid){
+            $req=$BDD->prepare("INSERT INTO relation (id_demandeur,id_receveur,statut) VALUES (?,?,?)");
+            $req->execute(array($id_demandeur,$id_receveur,1));
 
-            if(isset($verif_relation['id'])){/*follow existe*/
-                $valid=false;
-                echo "follow existe";
-            }
-            if($valid){
-                $req=$BDD->prepare("INSERT INTO relation (id_demandeur,id_receveur,statut) VALUES (?,?,?)");
-                $req->execute(array($id_demandeur,$id_receveur,1));
-
-            }
-
-            header('Location: profils.php?profil_id='.$id_receveur);
-            exit;
         }
 
-        else if(isset($_POST['unfollow'])){
-            echo "#unfollow";
-            $req=$BDD->prepare("DELETE FROM relation  WHERE (id_receveur = ? AND id_demandeur = ?)");
-            $req->execute(array($id_receveur,$id_demandeur));
-            header('Location: profils.php?profil_id='.$id_receveur);
-            exit;
-        }
-        else if(isset($_POST['bloquer'])){
-            echo "#bloquer";
-            $req=$BDD->prepare("DELETE FROM relation  WHERE (id_receveur = ? AND id_demandeur = ?) OR (id_receveur = ? AND id_demandeur = ?)");
-
-            $req->execute(array($id_receveur, $id_demandeur, $id_demandeur,$id_receveur));
-
-            $req=$BDD->prepare("INSERT INTO relation (id_demandeur,id_receveur, statut) VALUES (?,?,?)");
-            $req->execute(array($id_demandeur,$id_receveur,3));
-            /*c'est comme unfollow mais on insère juste l'id de du profil bloqué*/ /*on suppose que le statut 3 est une demande bloqué*/
-
-
-            header('Location: profils.php?profil_id='.$id_receveur);
-            exit;
-        } 
-        else if(isset($_POST['debloquer'])){
-            echo "#debloquer";
-            $req=$BDD->prepare("DELETE FROM relation  WHERE (id_demandeur = ? AND id_receveur = ? AND statut = ? )");
-            $req->execute(array($id_demandeur,$id_receveur,3));
-            header('Location: profils.php?profil_id='.$id_receveur);
-            exit;
-        } 
-
+        header('Location: profils.php?profil_id='.$id_receveur);
+        exit;
     }
 
+    else if(isset($_POST['unfollow'])){
+        echo "#unfollow";
+        $req=$BDD->prepare("DELETE FROM relation  WHERE (id_receveur = ? AND id_demandeur = ?)");
+        $req->execute(array($id_receveur,$id_demandeur));
+        header('Location: profils.php?profil_id='.$id_receveur);
+        exit;
+    }
+    else if(isset($_POST['bloquer'])){
+        echo "#bloquer";
+        $req=$BDD->prepare("DELETE FROM relation  WHERE (id_receveur = ? AND id_demandeur = ?) OR (id_receveur = ? AND id_demandeur = ?)");
 
+        $req->execute(array($id_receveur, $id_demandeur, $id_demandeur,$id_receveur));
+
+        $req=$BDD->prepare("INSERT INTO relation (id_demandeur,id_receveur, statut) VALUES (?,?,?)");
+        $req->execute(array($id_demandeur,$id_receveur,3));
+        /*c'est comme unfollow mais on insère juste l'id de du profil bloqué*/ /*on suppose que le statut 3 est une demande bloqué*/
+
+
+        header('Location: profils.php?profil_id='.$id_receveur);
+        exit;
+    } 
+    else if(isset($_POST['debloquer'])){
+        echo "#debloquer";
+        $req=$BDD->prepare("DELETE FROM relation  WHERE (id_demandeur = ? AND id_receveur = ? AND statut = ? )");
+        $req->execute(array($id_demandeur,$id_receveur,3));
+        header('Location: profils.php?profil_id='.$id_receveur);
+        exit;
+    } 
+}
 
 // req1 seletionne toutes les relations où on le follow
 $req1 = $BDD->prepare("SELECT *
-                            FROM relation
-                            WHERE id_receveur = ? AND statut = 1");
+                        FROM relation
+                        WHERE id_receveur = ? AND statut = 1");
 $req1->execute(array($id_receveur));
-<<<<<<< HEAD
-    // compter les resulter, count la tableau
-    $nb_follow=0;
+// compter les resulter, count la tableau
+$nb_follow=0;
 $resuFOLLOWprof= $req1->fetchAll();
 
 //print_r($resuRELA);
 foreach($resuFOLLOWprof as $rr){ foreach($rr as $key => $value){
-    =======
 
+    if($key =='statut' && $value== 1){ $nb_follow++;}   
+} }
 
-        $resuRELA = $req1->fetchAll();
-
-    // compter les resulter, count la tableau
-    $nb_follow=0;
-    foreach($resuRELA as $rr){
-        >>>>>>> a1ed8e49a8b9d9ce87e0ed770f9ec9a75674b417
-
-            if($key =='statut' && $value== 1){ $nb_follow++;}   
-    } }
-
-                                $okcmoncompte = false;
-                                if( $id_demandeur==$id_receveur){
-                                    $okcmoncompte = true;
-                                }
-                                <<<<<<< HEAD
-                                    =======
-
-                                    // VOUS A BLOUQER
-                                    $req2 = $BDD->prepare("SELECT *
-                            FROM relation
-                            WHERE id_demandeur = ? AND id_receveur = ? AND statut = 3");
-                                $req2->execute(array($id_receveur,$id_demandeur));
-
-
-                                $vousabloq = $req2->fetchAll();
-
-
-                                $okvousabloquer = false;
-                                if(isset($vousabloq['id_demandeur'])) {
-                                    $okvousabloquer = true;
-                                    echo '-111';
-                                }
-                                // VOUS LAVEZ LBOQUER
-                                $req2 = $BDD->prepare("SELECT *
-                            FROM relation
-                     WHERE id_demandeur = ? AND  id_receveur = ? AND  statut = 3");
-                                $req2->execute(array($id_demandeur,$id_receveur));
-
-
-
-                                $vouslaverabloq = $req2->fetch();
-                                var_dump($vouslaverabloq);
-                                print_r($vouslaverabloq);
-                                $okvouslavezbloquer = false;
-                                if(isset($vousabloq['id_demandeur'])) {
-                                    $okvouslavezbloquer = true;
-                                    echo '-222';
-                                }
-
-
-
-                                >>>>>>> a1ed8e49a8b9d9ce87e0ed770f9ec9a75674b417
+$okcmoncompte = false;
+if( $id_demandeur==$id_receveur){
+    $okcmoncompte = true;
+}
 ?>
 
 
@@ -216,7 +164,7 @@ foreach($resuFOLLOWprof as $rr){ foreach($rr as $key => $value){
         <meta charset="utf-8">
         <title>Profil de <?= $afficher_profil['user_pseudo'] ?></title>
         <?php
-                                    require_once('assets/skeleton/headLinkCSS.html');
+    require_once('assets/skeleton/headLinkCSS.html');
         ?>
         <link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
         <link rel="stylesheet" type="text/css" href="assets/css/search.css">
@@ -224,7 +172,6 @@ foreach($resuFOLLOWprof as $rr){ foreach($rr as $key => $value){
 
     </head>
     <body class="profile-page">
-
 
 
         <!--   *************************************************************  -->
@@ -299,21 +246,18 @@ foreach($resuFOLLOWprof as $rr){ foreach($rr as $key => $value){
 
                     <?php 
 
-
-                                $req = $BDD->prepare("  SELECT *
+                    $req = $BDD->prepare("  SELECT *
                                                         FROM beat
                                                         WHERE beat_author_id = ?
                                                         ORDER BY beat_dateupload DESC");
-                                $req->execute(array($id_receveur));
-                                $resuBEATS = $req->fetchAll();
+                    $req->execute(array($id_receveur));
+                    $resuBEATS = $req->fetchAll();
 
-
-                                $yadesresultatsBEATS = false;
-                                if (isset($resuBEATS) && !empty($resuBEATS)){
-                                    $yadesresultatsBEATS = true;
-                                }
+                    $yadesresultatsBEATS = false;
+                    if (isset($resuBEATS) && !empty($resuBEATS)){
+                        $yadesresultatsBEATS = true;
+                    }
                     ?>
-
 
                     <div class="row">
                         <div class="col-md-6 d-flex
@@ -321,7 +265,6 @@ foreach($resuFOLLOWprof as $rr){ foreach($rr as $key => $value){
                             <?php  require_once('assets/skeleton/tableBeatSearch.php'); ?>
                         </div>
                     </div>
-
 
 
                 </div>
@@ -333,9 +276,8 @@ foreach($resuFOLLOWprof as $rr){ foreach($rr as $key => $value){
         <!--   *************************************************************  -->
         <!--   ************************** MUSIC PLAYER  **************************  -->
         <?php
-                                require_once('assets/skeleton/AudioPlayer/audioplayer.php');
+        require_once('assets/skeleton/AudioPlayer/audioplayer.php');
         ?>
-
 
     </body>
 </html>
